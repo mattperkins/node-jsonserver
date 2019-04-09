fbea80646d7349851f241bf68bbe33f1c4262dca
mattperkins <github@mattperkins.com>
2019-04-09 15:30:10 +0100

    basic express app

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('Hey, I\'m a Node.js app!')
+  res.send('App is running...!!')
 })
 
 app.listen(3000, () => {
-  console.log('Server is up on 3000')
+  console.log('Server is up on 3000!')
 })
